2bdd41e43c5cda6983dc26c500bc91ac63376226
ejaakml <eximindotong@yahoo.com>
2020-12-04 21:44:00 +0700

    555

---
+++ src/uts/if2/pkg10119061/rheizaakmalrazzaky/no/pkg3/UTSIF210119061RHEIZAAKMALRAZZAKYNo3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119061.rheizaakmalrazzaky.no.pkg3;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class UTSIF210119061RHEIZAAKMALRAZZAKYNo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
